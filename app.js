a4438b725c80255cb55f48dcde17f5ec26615890
fjordani <faljordani+github@gmail.com>
2021-08-02 09:45:56 +0200

    add navtabs

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+// const { exec } = require('child_process');
+
+// exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
+//     if (err) {
+//         console.log(err);
+//     }
+
+//     if (typeof stdout === 'string' && (stdout.trim() === 'master')) {
+//       console.log(`The branch is master`);
+//       // Call your function here conditionally as per branch
+//     }
+// });
 
 // use env vars
 require('dotenv').config();
